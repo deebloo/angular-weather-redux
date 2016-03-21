764f96e89910a346605d0508d2276749235ccb86
dblue <dannybluedesign@gmail.com>
2016-03-21 10:11:56 -0400

    add missing http

--- src/app/services/weather/weather.factory.js
+++ src/app/services/weather/weather.factory.js
@@ -8,7 +8,7 @@ import store from 'common/store';
  * @description
  * Factory for searching for weather.
  */
-function weather() {
+function weather($http) {
 	const getAPi = cityState => {
 		return `http://api.openweathermap.org/data/2.5/forecast/daily?appid=585e670f55ee9b114fa2f1f2731177d9&q=${cityState}&units=imperial&cnt=5`;
 	};
